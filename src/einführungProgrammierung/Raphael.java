bdce40b30677b2f16c6c31acabb3e3c5bfb66c6f
Raphael <rgeier07@gmail.com>
2025-10-29 15:21:57 +0100

    Uebung 2

--- src/einführungProgrammierung/Raphael.java
+++ src/einführungProgrammierung/Raphael.java
@@ -1,3 +1,12 @@
-//Test
 package einführungProgrammierung;
+  import java.util.Scanner;
 
+public class Raphael {
+	  public static void main (String args[]) {
+		  Scanner s = new Scanner(System.in);
+		  String zeile = new String();
+		  zeile = s.nextLine();
+		  System.out.println(zeile);
+		  s.close();
+     }
+}
